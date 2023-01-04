479c1a137614bcaf605ae00b3afa88bbbd69cd36
Armando <borelliarmando12@gmail.com>
2023-01-04 04:38:04 -0500

    Added grayscale & contrast enhancer to image in get_stats.py

--- get_stats.py
+++ get_stats.py
@@ -1,7 +1,36 @@
-from PIL import Image
+from PIL import Image, ImageEnhance
 import pytesseract
 import csv
 
-# Simple image to string
-print(pytesseract.image_to_string(Image.open('./assets/label-images/a-j-alexy.png')))
+# Image to string
+# print(pytesseract.image_to_string(Image.open("./assets/label-images/a-j-alexy.png")))
+
+# Load image
+image = Image.open("./assets/label-images/a-j-alexy.png")
+
+
+# Examine file contents
+print(image.format, image.size, image.mode)
+
+
+# Convert the image to grayscale
+image = image.convert('L')
+
+# Save the grayscale image
+image.save('image_grayscale.jpg')
+
+
+# Create an ImageEnhancer object
+enhancer = ImageEnhance.Contrast(image)
+
+# Adjust the contrast
+image = enhancer.enhance(1.5)
+
+# Save the enhanced image
+image.save('image_enhanced.jpg')
+
+
+
+
+# print(pytesseract.image_to_string(Image.open("image_enhanced.jpg")))
 

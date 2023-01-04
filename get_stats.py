53d5a0986ea04b487934791a5677cfc66976b65d
Armando <borelliarmando12@gmail.com>
2023-01-04 05:44:23 -0500

    Edited get_stats.py with image crop for better view by ocr engine

--- get_stats.py
+++ get_stats.py
@@ -1,36 +1,30 @@
-from PIL import Image, ImageEnhance
+from PIL import Image, ImageEnhance, ImageFilter
 import pytesseract
 import csv
 
-# Image to string
-# print(pytesseract.image_to_string(Image.open("./assets/label-images/a-j-alexy.png")))
 
 # Load image
-image = Image.open("./assets/label-images/a-j-alexy.png")
-
-
-# Examine file contents
-print(image.format, image.size, image.mode)
+image = Image.open("./assets/label-images/aaron-judge.png")
 
 
 # Convert the image to grayscale
 image = image.convert('L')
+# image.save('image_grayscale.jpg')
 
-# Save the grayscale image
-image.save('image_grayscale.jpg')
+# Crop the image
+# 						(left, upper, right, lower)
+cropped_image = image.crop((0, 350, 2000, 2000))
+cropped_image.save('cropped_image.jpg')
 
-
-# Create an ImageEnhancer object
-enhancer = ImageEnhance.Contrast(image)
-
-# Adjust the contrast
-image = enhancer.enhance(1.5)
-
-# Save the enhanced image
-image.save('image_enhanced.jpg')
+# Image to string
+print(pytesseract.image_to_string(Image.open("cropped_image.jpg")))
 
 
-
+# # Create an ImageEnhancer object
+# enhancer = ImageEnhance.Contrast(cropped_image)
+# # Adjust the contrast
+# cropped_image = enhancer.enhance(5)
+# cropped_image.save('image_enhanced.jpg')
 
 # print(pytesseract.image_to_string(Image.open("image_enhanced.jpg")))
 

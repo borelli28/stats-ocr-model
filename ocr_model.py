008d91aad5df9e6d210cf67256ca4ac51b0d6c03
Armando <borelliarmando12@gmail.com>
2023-01-05 14:11:25 -0500

    Re adapting ocr_model.py for pyteserract

--- ocr_model.py
+++ ocr_model.py
@@ -1,54 +1,12 @@
-from sklearn import svm
 from PIL import Image
 import json
 import numpy as np
+import pytesseract
 
+pytesseract.pytesseract.tesseract_cmd = '/path/to/tesseract'
 
-def load_data():
-    # Open the images and get the pixel values as 2D arrays
-    image_one = Image.open('./assets/labeled-images/a-j-alexy.png')
-    pixels_one = np.array(image_one)
-    image_two = Image.open('./assets/labeled-images/aaron-judge.png')
-    pixels_two = np.array(image_two)
+# Set the path to the trained model
+tessdata_dir_config = '--tessdata-dir "/path/to/trained/model"'
 
-    # Stack the pixel arrays together
-    X = np.vstack((pixels_one, pixels_two))
-
-    # Load the annotations file
-    annotations = None
-    with open("./assets/annotations/2_coco_imglab.json", "r") as f:
-        annotations = json.load(f)
-
-    # Get the labels from the annotations
-    labels = []
-    for annotation in annotations["annotations"]:
-        if "label" in annotation:
-            labels.append(annotation["label"])
-
-    # Convert the labels to a numpy array
-    y = np.array(labels)
-
-    return X, y
-
-print(load_data())
-
-
-# Load the training data
-X_train, y_train = load_data()
-
-# Make sure that X_train and y_train have the same number of samples
-assert X_train.shape[0] == y_train.shape[0]
-
-# Create the SVM model
-svm_model = svm.SVC(kernel='linear')
-
-# Train the model on the training data
-svm_model.fit(X_train, y_train)
-
-# Test the model on the test data
-X_test, y_test = load_test_data()
-accuracy = svm_model.score(X_test, y_test)
-
-print(f"Test accuracy: {accuracy}")
-
-
+# Read the image and recognize the text
+text = pytesseract.image_to_string(image, lang='eng', config=tessdata_dir_config)

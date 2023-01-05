dc92cbe973485362af43e27a3927701bea6e36d8
Armando <borelliarmando12@gmail.com>
2023-01-05 05:49:03 -0500

    Switched back to sklearn svm model

--- ocr_model.py
+++ ocr_model.py
@@ -1,62 +1,54 @@
-import pytesseract
+from sklearn import svm
 from PIL import Image
 import json
-import sklearn
-from sklearn import model_selection, linear_model
 import numpy as np
 
 
-# Open an image and get its pixel values as a 2D array
-image_one = Image.open('./assets/labeled-images/a-j-alexy.png')
-pixels_one = np.array(image_one)
+def load_data():
+    # Open the images and get the pixel values as 2D arrays
+    image_one = Image.open('./assets/labeled-images/a-j-alexy.png')
+    pixels_one = np.array(image_one)
+    image_two = Image.open('./assets/labeled-images/aaron-judge.png')
+    pixels_two = np.array(image_two)
 
-image_two = Image.open('./assets/labeled-images/aaron-judge.png')
-pixels_two = np.array(image_two)
+    # Stack the pixel arrays together
+    X = np.vstack((pixels_one, pixels_two))
+
+    # Load the annotations file
+    annotations = None
+    with open("./assets/annotations/2_coco_imglab.json", "r") as f:
+        annotations = json.load(f)
+
+    # Get the labels from the annotations
+    labels = []
+    for annotation in annotations["annotations"]:
+        if "label" in annotation:
+            labels.append(annotation["label"])
+
+    # Convert the labels to a numpy array
+    y = np.array(labels)
+
+    return X, y
+
+print(load_data())
 
 
-# Load the annotations file
-annotations = None
-with open("./assets/annotations/2_coco_imglab.json", "r") as f:
-    annotations = json.load(f)
-    # print(annotations)
+# Load the training data
+X_train, y_train = load_data()
 
-# Create a mapping from category id to category name
-categories_dict = {}
-for category in annotations["categories"]:
-    categories_dict[category["id"]] = category["name"]
+# Make sure that X_train and y_train have the same number of samples
+assert X_train.shape[0] == y_train.shape[0]
 
-# Extract the labels from the annotations
-labels = []
-for annotation in annotations["annotations"]:
-    category_id = annotation["category_id"]
-    if category_id in categories_dict:
-        label = categories_dict[category_id]
-        labels.append(label)
-
-
-# Stack the pixel arrays together
-X = np.vstack((pixels_one, pixels_two))
-# Reshape the array into a 2D array of shape (n_samples, n_features)
-X = X.reshape(-1, pixels_one.size)
-y = labels
-
-print("X & y size:")
-print(len(X))
-print(len(labels))
-
-# Split the data into training and test sets
-X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=1, random_state=42)
-
-# Choose a classification algorithm and instantiate a model object
-model = sklearn.linear_model.LogisticRegression(max_iter=1000)
+# Create the SVM model
+svm_model = svm.SVC(kernel='linear')
 
 # Train the model on the training data
-model.fit(X_train, y_train)
+svm_model.fit(X_train, y_train)
 
-# Evaluate the model on the test data
-score = model.score(X_test, y_test)
-print(f"Model score: {score}".format(score))
+# Test the model on the test data
+X_test, y_test = load_test_data()
+accuracy = svm_model.score(X_test, y_test)
 
-
+print(f"Test accuracy: {accuracy}")
 
 

4cc29e7dd655d160e730e418226e1428aea8ec11
Armando <borelliarmando12@gmail.com>
2023-01-05 14:24:44 -0500

    Added pyteserract image_to_string() to ocr_mode.py

--- ocr_model.py
+++ ocr_model.py
@@ -3,10 +3,20 @@ import json
 import numpy as np
 import pytesseract
 
-pytesseract.pytesseract.tesseract_cmd = '/path/to/tesseract'
 
-# Set the path to the trained model
-tessdata_dir_config = '--tessdata-dir "/path/to/trained/model"'
+# Open the image
+image = Image.open("./assets/labeled-images/a-j-alexy.png")
 
-# Read the image and recognize the text
-text = pytesseract.image_to_string(image, lang='eng', config=tessdata_dir_config)
+
+# Reads the text in the image
+text = pytesseract.image_to_string(image)
+print(text)
+
+
+# pytesseract.pytesseract.tesseract_cmd = '/path/to/tesseract'
+
+# # Set the path to the trained model
+# tessdata_dir_config = '--tessdata-dir "/path/to/trained/model"'
+
+# # Read the image and recognize the text
+# text = pytesseract.image_to_string(image, lang='eng', config=tessdata_dir_config)

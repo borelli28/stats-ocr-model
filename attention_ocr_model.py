5da6d5e7bfabdb5295f6018a198dfd4e30c66dbe
Armando <borelliarmando12@gmail.com>
2023-01-11 04:09:09 -0500

    Added parse_annotations

--- attention_ocr_model.py
+++ attention_ocr_model.py
@@ -1,7 +1,11 @@
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
 import torch.optim as optim
+import xml.etree.ElementTree as ET
+from torch.nn import functional as F
+from torchvision import transforms
+import os
 
 
 class AttentionOCR(nn.Module):
@@ -12,29 +16,44 @@ class AttentionOCR(nn.Module):
         self.attn = nn.Linear(hidden_size * 2, 1)
         self.fc = nn.Linear(hidden_size * 2, num_classes)
 
+    def parse_annotations(self, xml_path):
+
+        labels = []
+        for filename in os.listdir(xml_path):
+            if filename.endswith(".xml"):
+                file_path = os.path.join(xml_path, filename)
+                tree = ET.parse(file_path)
+                root = tree.getroot()
+                for obj in root.findall('object'):
+                    label = obj.find('name').text
+                    xmin = int(float(obj.find('bndbox/xmin').text))
+                    ymin = int(float(obj.find('bndbox/ymin').text))
+                    xmax = int(float(obj.find('bndbox/xmax').text))
+                    ymax = int(float(obj.find('bndbox/ymax').text))
+                    labels.append({'label': label, 'xmin': xmin, 'ymin': ymin, 'xmax': xmax, 'ymax': ymax})
+        return labels
+
+    def create_dataloader(self, annotations_path, batch_size, transforms, shuffle=True):
+        
+        annotations = self.parse_annotations(annotations_path)
+        dataset = torch.utils.data.Dataset(annotations, transforms)
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+
+        return dataloader
+
     def forward(self, x):
-        # Pass the image through the convolutional layers
+        
         x = self.conv(x)
-        
-        # Apply a RNN to the features
         x, _ = self.rnn(x)
-        
-        # Apply the attention mechanism
         attn_weights = self.attn(x)
         attn_weights = attn_weights.squeeze(2)
         attn_weights = F.softmax(attn_weights, dim=1)
         x = x * attn_weights.unsqueeze(2)
-        
-        # Pass the features through the fully connected layer
         x = self.fc(x)
-        
         return x
 
-    def create_dataloader(dataset, batch_size, shuffle=True):
-        return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
-
-
-    def train_attention_ocr(model, dataloader, criterion, optimizer, num_epochs):
+    def train_attention_ocr(self, model, dataloader, criterion, optimizer, num_epochs):
+        
         model.train()
         for epoch in range(num_epochs):
             for i, (inputs, labels) in enumerate(dataloader):
@@ -57,9 +76,15 @@ model = AttentionOCR(input_size, hidden_size, num_classes)
 print(model)
 
 batch_size = 64
-train_dataset = "./assets/"
+train_dataset = "./assets/annotations"
 
-train_dataloader = model.create_dataloader(train_dataset, batch_size)
+transform = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                transforms.RandomRotation(10),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+train_dataloader = model.create_dataloader(train_dataset, batch_size, transform)
+
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters())
 num_epochs = 20

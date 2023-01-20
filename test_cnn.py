9dfe2ca72eb43791db898ffecb6e358d36ef99db
Armando <borelliarmando12@gmail.com>
2023-01-20 16:05:41 -0500

    Changed input channels for 1 cnn layer to 1

--- test_cnn.py
+++ test_cnn.py
@@ -1,12 +1,13 @@
 import torch
 from torch import nn
+from torchvision import transforms
+import torch.nn.functional as F
 from torchvision.transforms import ToTensor
 from torch.utils.data import Dataset, DataLoader
 import torch.optim as optim
 import xml.etree.ElementTree as ET
 import os
 from PIL import Image
-from torchvision import transforms
 
 
 transform = transforms.Compose([
@@ -62,40 +63,43 @@ class CustomDataset(Dataset):
 
         return image, label
 
+
 # Get cpu or gpu device for training.
 device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 print(f"Using {device} device")
 
+
 # Define model
-class NeuralNetwork(nn.Module):
-    def __init__(self):
+class CNN(nn.Module):
+    def __init__(self, num_classes):
         super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10)
-        )
+        # nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = self.flatten(x)
-        # logits = self.linear_relu_stack(x)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
-
-
-model = NeuralNetwork().to(device)
-print(model)
-
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
 
 def train(annotations_path, images_path, batch_size, num_epochs):
     # Initialize CustomDataset class in order to extract the num_classes
     dataset = CustomDataset(annotations_path, images_path)
     num_classes = len(dataset.classes)
+
+    model = CNN(num_classes).to(device)
+
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
     # Define the loss function and optimizer
     criterion = nn.CrossEntropyLoss()

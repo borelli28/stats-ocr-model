bc1a3b1d970665e364acc0b9c6371ab910fb4eee
Armando <borelliarmando12@gmail.com>
2023-01-24 16:24:06 -0500

    Increased batch_size to 250

--- cnn_ocr_model.py
+++ cnn_ocr_model.py
@@ -140,7 +140,7 @@ transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
-batch_size = 64
+batch_size = 250
 num_epochs = 5
 annotations_path = "./assets/annotations"
 images_path = "./assets/labeled-images"

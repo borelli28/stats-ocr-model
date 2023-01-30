19007d88efe48e27e0f1ed87ba18d8111b263715
armando <borelliarmando28@gmail.com>
2023-01-30 06:24:06 -0500

    Clean out some minor stuff

--- cnn_ocr_model.py
+++ cnn_ocr_model.py
@@ -138,11 +138,13 @@ def test(model, test_dataloader, loss_function):
     num_batches = len(test_dataloader)
     model.eval()
     test_loss, correct = 0, 0
+
     with torch.no_grad():
         for X, y in test_dataloader:
             pred = model(X)
             test_loss += loss_function(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
     test_loss /= num_batches
     correct /= size
     print(f"Model performance test data: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")

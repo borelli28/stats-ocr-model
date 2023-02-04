63d7d57a6de42f8b2994e83aeb643534aa296c57
Armando <borelliarmando12@gmail.com>
2023-02-04 00:41:14 -0500

    Changed svm model to linear for better performance

--- train-models/svm_ocr_model.py
+++ train-models/svm_ocr_model.py
@@ -182,7 +182,7 @@ def train_model(X, y):
     # Split the data into training and test sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-    clf = SVC()
+    clf = SVC(kernel="linear")
 
     # Train the classifier on the training data
     clf.fit(X_train, y_train)

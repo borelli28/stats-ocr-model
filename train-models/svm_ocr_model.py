7cb09bafab2dbcce5a2bfdb18ec1590d18558083
Armando <borelliarmando28@gmail.com>
2023-02-05 07:39:16 -0500

    Got rid of commented code

--- train-models/svm_ocr_model.py
+++ train-models/svm_ocr_model.py
@@ -208,40 +208,6 @@ def train_model(X, y):
     by the model on the validation data.
     """
 
-    """
-    Model image prediction experiment
-    """
-    # image_path = "../assets/labeled-images/aaron-judge.png"
-    # image = Image.open(image_path)
-
-    # # Convert the image to a NumPy array with a consistent shape
-    # image_array = np.array(image, dtype=np.float32)
-    # image_array = image_array.reshape((1,) + image_array.shape)
-
-    # # Extract features from the image
-    # features = extract_features(image_array)
-
-    # print("\n shape")
-    # print(features.shape)
-    # print("\n")
-
-    # features = X.reshape(X.shape[0], -1)
-
-    # print("\n shape")
-    # print(features.shape)
-    # print("\n")
-
-    # # Make a prediction using the model
-    # prediction = clf.predict(features)
-    # print("\npredictioooon:")
-    # print(prediction)
-    # print(prediction.size)
-    # print(type(prediction))
-    # print("\n")
-
-    # for i in range(len(prediction)):
-    #     print(prediction[i])
-
     return clf
 
 

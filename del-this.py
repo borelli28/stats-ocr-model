bec86a3847675e18b6f9cccdc2bac978b858a0a7
Armando <borelliarmando28@gmail.com>
2023-02-12 14:34:40 -0500

    WIP

--- del-this.py
+++ del-this.py
@@ -2,9 +2,7 @@ import cv2
 import numpy as np
 import xml.etree.ElementTree as ET
 
-
 img_path = "./assets/labeled-images/1.png"
-
 img = cv2.imread(img_path)
 
 # Convert the image to grayscale
@@ -16,10 +14,11 @@ gray = cv2.GaussianBlur(gray, (5, 5), 0)
 # Threshold the image to convert it to binary
 thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-# Find contours in the binary image
-contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# Perform vertical projection to find the location of each line of numbers
+vertical_projection = np.sum(thresh, axis=1)
+y_coords = np.where(vertical_projection > 0)[0]
 
-# Create the XML tree and root element
+# Crop each line of numbers and find contours
 root = ET.Element("annotation")
 ET.SubElement(root, "folder").text = "images"
 ET.SubElement(root, "filename").text = "mike-brosseau.png"
@@ -33,25 +32,38 @@ ET.SubElement(size, "height").text = str(height)
 ET.SubElement(size, "depth").text = str(depth)
 ET.SubElement(root, "segmented").text = "0"
 
-# Loop through each contour and add it as an object to the XML tree
-for contour in contours:
-    # Get the bounding box for the contour
-    x, y, w, h = cv2.boundingRect(contour)
-    
-    # Create the XML element for the object
-    obj = ET.SubElement(root, "object")
-    ET.SubElement(obj, "name").text = "Unspecified"
-    ET.SubElement(obj, "pose").text = "Unspecified"
-    ET.SubElement(obj, "truncated").text = "0"
-    ET.SubElement(obj, "difficult").text = "0"
-    bbox = ET.SubElement(obj, "bndbox")
-    ET.SubElement(bbox, "xmin").text = str(x)
-    ET.SubElement(bbox, "ymin").text = str(y)
-    ET.SubElement(bbox, "xmax").text = str(x + w)
-    ET.SubElement(bbox, "ymax").text = str(y + h)
-    
-    # Draw the bounding box on the image
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+for i in range(len(y_coords) - 1):
+    y1 = y_coords[i]
+    y2 = y_coords[i + 1]
+
+    line_img = img[y1:y2, :]
+    line_gray = gray[y1:y2, :]
+    line_thresh = thresh[y1:y2, :]
+
+    # Find contours in the grayscale image
+    contours, _ = cv2.findContours(gray, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Loop through each contour and add it as an object to the XML tree
+    for contour in contours:
+        # Get the bounding box for the contour
+        x, y, w, h = cv2.boundingRect(contour)
+
+        # Check if the contour is small enough to be a digit
+        if w < img.shape[1] // 2 and h < img.shape[0] // 2:
+            # Create the XML element for the object
+            obj = ET.SubElement(root, "object")
+            ET.SubElement(obj, "name").text = "digit"
+            ET.SubElement(obj, "pose").text = "Unspecified"
+            ET.SubElement(obj, "truncated").text = "0"
+            ET.SubElement(obj, "difficult").text = "0"
+            bbox = ET.SubElement(obj, "bndbox")
+            ET.SubElement(bbox, "xmin").text = str(x)
+            ET.SubElement(bbox, "ymin").text = str(y)
+            ET.SubElement(bbox, "xmax").text = str(x + w)
+            ET.SubElement(bbox, "ymax").text = str(y + h)
+
+        # Draw the bounding box on the image
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 # Save the annotated image
 cv2.imwrite("annotated_image.png", img)

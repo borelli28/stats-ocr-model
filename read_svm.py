60d5b1b204f7162a0a1971c5cae5f6141f336a94
Armando <borelliarmando28@gmail.com>
2023-02-16 18:10:46 -0500

    Move code into draw_image()

--- read_svm.py
+++ read_svm.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 
 def create_annotation(image_path, object_name, xmin, ymin, xmax, ymax, width, height):
-    # create annotation object
+    # create annotation element
     annotation = ET.Element("annotation")
 
     # create sub elements
@@ -36,16 +36,16 @@ def create_annotation(image_path, object_name, xmin, ymin, xmax, ymax, width, he
     segmented.text = "0"
 
     # create object element
-    object = ET.SubElement(annotation, "object")
-    name = ET.SubElement(object, "name")
+    obj = ET.SubElement(annotation, "object")
+    name = ET.SubElement(obj, "name")
     name.text = object_name
-    pose = ET.SubElement(object, "pose")
+    pose = ET.SubElement(obj, "pose")
     pose.text = "Unspecified"
-    truncated = ET.SubElement(object, "truncated")
+    truncated = ET.SubElement(obj, "truncated")
     truncated.text = "0"
-    difficult = ET.SubElement(object, "difficult")
+    difficult = ET.SubElement(obj, "difficult")
     difficult.text = "0"
-    bndbox = ET.SubElement(object, "bndbox")
+    bndbox = ET.SubElement(obj, "bndbox")
     xmin_element = ET.SubElement(bndbox, "xmin")
     xmin_element.text = str(xmin)
     ymin_element = ET.SubElement(bndbox, "ymin")
@@ -153,48 +153,57 @@ def extract_data():
     return X, y
 
 
+def draw_image(image_path):
+    image = cv2.imread(image_path)
+
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Apply Otsu's thresholding
+    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+    # Find contours in the binary image
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Extract the regions of the image containing text
+    text_regions = []
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        text_regions.append(image[y:y+h, x:x+w])
+
+    # Process each text region with your OCR model
+    for i, region in enumerate(text_regions):
+        # Use your SVM OCR trained model to predict the labels for this region
+        # labels = svm_model.predict(region)
+
+        # Draw a rectangle around the text region
+        contour = contours[i]
+        if len(contour) >= 4:
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+            # Create annotation file for this object
+            object_name = "text_region"  # replace with appropriate object name
+            annotation = create_annotation(image_path, object_name, x, y, x+w, y+h, image.shape[1], image.shape[0])
+            print(annotation)
+
+            # Add the predicted labels as text within the rectangle
+            # label_text = ''.join(labels)
+            # cv2.putText(image, label_text, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+
+    # Display the image with text regions and labels
+    cv2.imshow("Text Regions with Labels", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 svm_model = joblib.load("./models/svm_model.pkl")
 
 # Load the image
 image_path = "./assets/labeled-images/jazz-chisholm-jr.png"
-image = cv2.imread(image_path)
 
-# Convert the image to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+draw_image(image_path)
 
-# Apply Otsu's thresholding
-ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-# Find contours in the binary image
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# Extract the regions of the image containing text
-text_regions = []
-for contour in contours:
-    x, y, w, h = cv2.boundingRect(contour)
-    text_regions.append(image[y:y+h, x:x+w])
 
-# Process each text region with your OCR model
-for i, region in enumerate(text_regions):
-    # Use your SVM OCR trained model to predict the labels for this region
-    # labels = svm_model.predict(region)
 
-    # Draw a rectangle around the text region
-    contour = contours[i]
-    if len(contour) >= 4:
-        x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-        # Create annotation file for this object
-        object_name = "text_region"  # replace with appropriate object name
-        annotation = create_annotation(image_path, object_name, x, y, x+w, y+h, image.shape[1], image.shape[0])
-        print(annotation)
-
-        # Add the predicted labels as text within the rectangle
-        # label_text = ''.join(labels)
-        # cv2.putText(image, label_text, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
-
-# Display the image with text regions and labels
-cv2.imshow("Text Regions with Labels", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
